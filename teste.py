75c591ee896c4801f973980eb35b29ef156895a3
Folistw <134406466+Folistw@users.noreply.github.com>
2023-05-23 15:45:13 +0100

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from PIL import Image
 
-image = Image.open('1.png')
-st.image(image)
 st.title("VetVisita")
 st.write("Veterinária ao domicílio ")
 st.write("Whatsapp 919595959595 ")
