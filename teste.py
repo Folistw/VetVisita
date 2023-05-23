9d02972dfdfe88906fb0fe30df02cee07d3f3d13
Folistw <134406466+Folistw@users.noreply.github.com>
2023-05-23 15:42:57 +0100

    Create teste.py

---
+++ teste.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from PIL import Image
+
+image = Image.open('1.png')
+st.image(image)
+st.title("VetVisita")
+st.write("Veterinária ao domicílio ")
+st.write("Whatsapp 919595959595 ")
+st.write("Instagram @VetVisita ")
