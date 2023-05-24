ab274c21c1c83325924d5d556a7c45cdefccf501
Folistw <134406466+Folistw@users.noreply.github.com>
2023-05-24 16:45:39 +0100

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,7 +2,7 @@ import plotly.graph_objects as go
 import pandas as pd
 import streamlit as st
 
-colnames=['Month', 'Visits'] 
+colnames=['Month', 'Visits', 'Unique_users'] 
 df = pd.read_csv("Data.csv", sep=',', names=colnames, header=None)
 
 # Step 3: Prepare your data
@@ -21,6 +21,26 @@ fig.update_layout(
     title='Visits per month',
     xaxis_title='Months',
     yaxis_title='Visits'
+)
+
+st.plotly_chart(fig)
+
+# Step 3: Prepare your data
+categories = df['Month'][1:]
+heights = df['Unique_users'][1:]
+
+# Step 4: Create a bar chart object
+bar_chart = go.Bar(x=categories, y=heights)
+
+# Step 5: Create a figure and add the bar chart object
+fig = go.Figure()
+fig.add_trace(bar_chart)
+
+# Step 6: Customize the chart (optional)
+fig.update_layout(
+    title='Unique users per month',
+    xaxis_title='Months',
+    yaxis_title='Unique Users'
 )
 
 st.plotly_chart(fig)
