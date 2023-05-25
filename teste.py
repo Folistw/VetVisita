c4cd82002de9d8d1c32cbf5f2b14d8de54e7cfc7
Folistw <134406466+Folistw@users.noreply.github.com>
2023-05-25 11:56:11 +0100

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,45 +2,48 @@ import plotly.graph_objects as go
 import pandas as pd
 import streamlit as st
 
-colnames=['Month', 'Visits', 'Unique_users'] 
-df = pd.read_csv("Data.csv", sep=',', names=colnames, header=None)
+if st.experimental_user.email == "pedro.gomes.fccn@gmail.com":
+    st.write("Tu es Nabo")
+else:
+    colnames=['Month', 'Visits', 'Unique_users'] 
+    df = pd.read_csv("Data.csv", sep=',', names=colnames, header=None)
 
-# Step 3: Prepare your data
-categories = df['Month'][1:]
-heights = df['Visits'][1:]
+    # Step 3: Prepare your data
+    categories = df['Month'][1:]
+    heights = df['Visits'][1:]
 
-# Step 4: Create a bar chart object
-bar_chart = go.Bar(x=categories, y=heights)
+    # Step 4: Create a bar chart object
+    bar_chart = go.Bar(x=categories, y=heights)
 
-# Step 5: Create a figure and add the bar chart object
-fig = go.Figure()
-fig.add_trace(bar_chart)
+    # Step 5: Create a figure and add the bar chart object
+    fig = go.Figure()
+    fig.add_trace(bar_chart)
 
-# Step 6: Customize the chart (optional)
-fig.update_layout(
-    title='Visits per month',
-    xaxis_title='Months',
-    yaxis_title='Visits'
-)
+    # Step 6: Customize the chart (optional)
+    fig.update_layout(
+        title='Visits per month',
+        xaxis_title='Months',
+        yaxis_title='Visits'
+    )
 
-st.plotly_chart(fig)
+    st.plotly_chart(fig)
 
-# Step 3: Prepare your data
-categories = df['Month'][1:]
-heights = df['Unique_users'][1:]
+    # Step 3: Prepare your data
+    categories = df['Month'][1:]
+    heights = df['Unique_users'][1:]
 
-# Step 4: Create a bar chart object
-bar_chart = go.Bar(x=categories, y=heights)
+    # Step 4: Create a bar chart object
+    bar_chart = go.Bar(x=categories, y=heights)
 
-# Step 5: Create a figure and add the bar chart object
-fig = go.Figure()
-fig.add_trace(bar_chart)
+    # Step 5: Create a figure and add the bar chart object
+    fig = go.Figure()
+    fig.add_trace(bar_chart)
 
-# Step 6: Customize the chart (optional)
-fig.update_layout(
-    title='Unique users per month',
-    xaxis_title='Months',
-    yaxis_title='Unique Users'
-)
+    # Step 6: Customize the chart (optional)
+    fig.update_layout(
+        title='Unique users per month',
+        xaxis_title='Months',
+        yaxis_title='Unique Users'
+    )
 
-st.plotly_chart(fig)
+    st.plotly_chart(fig)
